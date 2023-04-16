82dbf9a4945fd0133b0920a04b1543b45a6f395f
OleksiyMunenko <oleksiymun@gmail.com>
2023-04-16 19:56:22 +0300

    fanish

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,10 @@
-import { Oval } from  'react-loader-spinner'
+import { Oval } from  'react-loader-spinner';
+import css from './Loader.module.css';
 
 const Loader = () => {
 	return (
-		<Oval
+		<div className={css.spinner}>
+			<Oval
 			height={80}
 			width={80}
 			color="#4fa94d"
@@ -14,6 +16,8 @@ const Loader = () => {
 			strokeWidth={2}
 			strokeWidthSecondary={2}
 		/>
+		</div>
+		
 	)
 }
 
